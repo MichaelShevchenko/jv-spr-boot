7a032c7143a657cde22498678d6fca9a1d9d054c
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-11-10 04:51:25 +0200

    initial commit

---
+++ src/main/java/spring/boot/com/sbparser/SbParserApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.com.sbparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/boot/com/sbparser/SbParserApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.com.sbparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
